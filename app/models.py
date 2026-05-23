54582c1c71f3756281927285ed30c0562c99d39a
WILDxMSD <godaraankit939@gmail.com>
2026-05-23 20:47:26 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,6 @@
+from django.db import models
 
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    course = models.CharField(max_length=100)
