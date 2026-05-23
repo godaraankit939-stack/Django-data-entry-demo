ef759eccb7147ea76d91f3e43d22fab484b32208
WILDxMSD <godaraankit939@gmail.com>
2026-05-23 20:47:48 +0530

    Implement form_view for student data handling
    Add form_view to handle student data submission.

--- app/views.py
+++ app/views.py
@@ -1,1 +1,12 @@
+from django.shortcuts import render
+from .models import Student
 
+def form_view(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        phone = request.POST['phone']
+        course = request.POST['course']
+
+        Student.objects.create(name=name, phone=phone, course=course)
+
+    return render(request, 'form.html')
